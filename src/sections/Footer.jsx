68497868e9d94988b5b54ffd37fec16bd4b412fc
Abhinav U <129955044+abhinavhh@users.noreply.github.com>
2024-07-31 00:48:20 +0530

    Update Footer.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
             ))}
           </div>
         </div>
-        <div className='flex flex-1 justify-between lg:gap-10 gap-20 fex-wrap'>
+        <div className='flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap'>
           {footerLinks.map((section) => (
             <div key={section}>
                <h4 className='text-white font-montserrat text-2xl leading-normal font-medium mb-6'>
